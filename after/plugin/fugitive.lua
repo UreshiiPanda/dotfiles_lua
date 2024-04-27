b4db02cfc142a62f71dd9f9545848d262b9ac6c5
UreshiiPanda <jojohoughton22@gmail.com>
2024-04-27 00:31:44 -0400

    keymappings added to fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,10 @@
-vim.keymap.set("n", "<leader>gi", vim.cmd.Git)
+-- pull up Fugitive's "git status" buffer
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- git push
+vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git('push') end, opts)
+
+-- these are for vDiff (vd) when performing a merge conflict btw 2 options
+-- and you can choose R side with "gl" and L side with "gh"
+vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
