b205ff12159a7f70995890a0692f5f23464c058a
UreshiiPanda <jojohoughton22@gmail.com>
2024-05-02 14:36:51 -0400

    changed LSP keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,15 +5,28 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local opts = {buffer = event.buf}
 
+    -- goto definition
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- show quick info on item under hover
+    vim.keymap.set('n', '<leader>I', function() vim.lsp.buf.hover() end, opts)
+    -- this is a way that the LSP can detect symbols across your entire curr workspace, 
+    -- instead of just curr file. But some LSPs can't do this or need some sort of setup
+    -- file to do this  (eg: JS has a package.json which allows the LSP to detect the curr
+    -- "workspace" and all files/symbols in it
+    vim.keymap.set('n', '<leader>vw', function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- open a quick diagnostic info on error/warning under the cursor
     vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    -- goto next diagnostic in file
     vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    -- goto prev diagnostic in file
     vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    -- see which code-actions the LSP server offers for the item under the cursor
+    -- some LSPs do not provide any code actions
+    vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+    -- show all refs to the item under the cursor in this file
+    vim.keymap.set('n', '<leader>vr', function() vim.lsp.buf.references() end, opts)
+    -- rename the var under the cursor everywhere it occurs in the file
+    vim.keymap.set('n', '<leader>vn', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
   end,
 })
@@ -73,11 +86,13 @@ cmp.setup({
     {name = 'luasnip', keyword_length = 2},
     {name = 'buffer', keyword_length = 3},
   },
+  -- these are for the dropdown menu that appears for code completion suggestions
+  -- the cmp.mapping.complete shows the entire list of possibilities
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<D-l>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<D-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<D-i>'] = cmp.mapping.confirm({ select = true }),
+    ['<D-f>'] = cmp.mapping.complete(),
   }),
   snippet = {
     expand = function(args)
